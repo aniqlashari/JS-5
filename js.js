7987cab416ab0d11f9286118238ff31ece42697f
aniqlashari <mahaqamber3@gmail.com>
2024-07-04 20:58:10 +0500

    first commit

---
+++ js.js
@@ -0,0 +1,75 @@
+// var gmail = prompt ("enter your gmail")
+
+
+
+// if (gmail === "aniq@gmail.com" ){
+
+// var password = prompt ("enter your password")
+
+
+
+
+// if (password === "admin"){
+
+
+// alert ("you are aniq")
+
+
+// }
+
+// }
+
+
+
+
+var schools =  ["native", "magdumia" ,"allied " ,"scoller" , "usman" , "alrehman " , "iqra" ]
+var school =  prompt("enter any school name")
+
+if(schools[0] === school){
+
+
+alert ("this school is best for kids    "+ schools[0])
+
+}
+else if(schools[1] === school){
+
+
+    alert ("this school is best for kids      "+ schools[1])
+    
+    }
+    else if(schools[2] === school){
+
+
+        alert ("this school is best for kids     "+ schools[2])
+        
+        }
+        else if(schools [3] === school){
+
+
+            alert ("this school is best for kids      "+ schools[3])
+            
+            }
+            else if(schools [4] === school){
+
+
+                alert ("this school is best for kids     "+ schools[4])
+                
+                }
+                else if(schools [5] === school){
+
+
+                    alert ("this school is best for kids     "+ schools[5])
+                    
+                    }
+                    else if(schools [6] === school){
+
+
+                        alert ("this school is best for kids    "+ schools[6])
+                        
+                        }
+   else {
+
+alert ("this school is not in list")
+
+
+   }                     
